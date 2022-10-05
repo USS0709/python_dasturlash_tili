57b72d4bb88251cae642d1b7220051748089acac
USS0709 <114044994+USS0709@users.noreply.github.com>
2022-10-05 07:16:59 +0300

    Add files via upload

---
+++ umidjon_amaliyish1.py
@@ -0,0 +1,9 @@
+### 13-misol ### Jaloldinov Umidjon
+import math
+from math import sqrt
+
+a=int(input('a kateti= '))
+b=int(input('b kateti= '))
+c=sqrt(a*a+b*b)
+s=a*b/2
+print(f'gipotenuzasi= {c} \n yuzasi= {s}')
